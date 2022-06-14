4bd2d443b7eb49a449bca8210a07d007926ed847
Fabio <fabioo134@gmail.com>
2022-06-14 01:13:57 -0300

    Adicionado ex101 ao 106

---
+++ mundo3/ex104-validando-entrada-dados-python.py
@@ -0,0 +1,18 @@
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        num = str(input(msg))
+        if num.isnumeric():
+            valor = int(num)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+# Programa principal
+n = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
